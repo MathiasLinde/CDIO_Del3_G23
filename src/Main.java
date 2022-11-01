fd26f6510162c939e1f57512172e9c583acfd9cc
AugustV <August@volsing.dk>
2022-11-01 15:48:47 +0100

    Test 1.1

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("smager rigtig flot");
+
+        System.out.println("jeg er så e9, smukke");
     }
 }
