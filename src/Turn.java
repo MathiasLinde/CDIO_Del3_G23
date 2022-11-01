210506fc8c47f174e846103859956eea3202515e
AugustV <August@volsing.dk>
2022-11-01 15:53:19 +0100

    Adding Turn class

---
+++ src/Turn.java
@@ -0,0 +1,27 @@
+public class Turn {
+
+    public class Turn {
+        /**
+         * Creates 2 die objects.
+         */
+        Tilegame.Die die1 = new Tilegame.Die();
+        Tilegame.Die die2 = new Die();
+        int sum;
+
+        /**
+         * Rolls 2 die and saves them in the variable sum.
+         *
+         * @return sum, returns the sum of the roll.
+         */
+
+        public int tur() {
+            die1.roll();
+            die2.roll();
+            sum = die1.getFaceValue() + die2.getFaceValue();
+
+
+            return sum;
+
+        }
+    }
+}
